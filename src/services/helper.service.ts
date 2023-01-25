330d12a15d6c58bec0c322c08e4a61a79762d5ed
Rornoa <joniker.e@mail.ru>
2023-01-25 18:19:46 +0300

    Changed background

--- src/services/helper.service.ts
+++ src/services/helper.service.ts
@@ -11,7 +11,7 @@ export class HelperService {
     switch (window.location.pathname){
       case '/login':
       case '/register': return "url('assets/backgrounds/background_start.jpg')"
-      case '/games-hub': return "url('assets/backgrounds/back_game_4.png')"
+      case '/games-hub': return "url('assets/backgrounds/back_game_3.png')"
       case '/game-table': return "url('assets/backgrounds/back_game_1.png')"
     }
     return  ''
